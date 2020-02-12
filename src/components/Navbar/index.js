73723fea6b56188d5499ed7d814a0ae6f984bb88
ignacioainol <iggnaxios@gmail.com>
2020-02-12 10:21:58 -0300

    added one menuList to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,43 +1,136 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Link from '@material-ui/core/Link';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from "@material-ui/styles";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
 
 const useStyles = makeStyles({
-    root: {
-      backgroundColor: 'transparent',
-      border: 0,
-      color: 'white',
-      height: 48,
-      fontWeight: 'bold',
-      padding: '0 30px',
+  buttonMod: {
+    backgroundColor: 'transparent',
+    border: 0,
+    color: 'white',
+    height: 40,
+    fontWeight: 'bold'
+  },
+});
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})(props => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
     },
-  });
+  },
+}))(MenuItem);
 
 export default function Navbar() {
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Link to="/" color={'inherit'}>
-                        <Button variant="contained" className={classes.root} disableElevation>
-                            Inicio
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Link to="/" color={'inherit'}>
+            <Button variant="contained" color="primary" className={classes.buttonMod} disableElevation>
+              Inicio
                         </Button>
-                    </Link>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+          </Link>
+
+          <Link to="/" color={'inherit'}>
+            <Button variant="contained" color="primary" className={classes.buttonMod} disableElevation>
+              Administración
+                        </Button>
+          </Link>
+
+          <Link to="/" color={'inherit'}>
+            <Button variant="contained" color="primary" className={classes.buttonMod} disableElevation>
+              Estadisticas
+                        </Button>
+          </Link>
+
+          <Link to="/" color={'inherit'}>
+            <Button variant="contained" color="primary" className={classes.buttonMod} disableElevation>
+              Comparación
+                        </Button>
+          </Link>
+
+          <Button
+            aria-controls="customized-menu"
+            className={classes.buttonMod}
+            onClick={handleClick}
+          >
+            Visualización
+          </Button>
+          <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <StyledMenuItem>
+              <ListItemIcon>
+                <SendIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Relaciones" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <DraftsIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Módulos" />
+            </StyledMenuItem>
+          </StyledMenu>
+
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
